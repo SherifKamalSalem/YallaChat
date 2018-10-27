53d6f5e111adb905f16a616f0707b01884aa5ce0
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-10-27 21:44:24 +0200

    Initial Commit

---
+++ YallaChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YallaChat
+//
+//  Created by Sherif Kamal on 10/27/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
