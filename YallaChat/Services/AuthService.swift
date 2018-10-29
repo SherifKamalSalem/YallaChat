3e0fe49313c133cc254540a25c46f43b86cf08bd
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-10-29 11:23:00 +0200

    Setup web services and mongoDB and start with creating user

---
+++ YallaChat/Services/AuthService.swift
@@ -0,0 +1,71 @@
+//
+//  AuthService.swift
+//  YallaChat
+//
+//  Created by Sherif Kamal on 10/29/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken : String {
+        
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail : String {
+        
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type" : "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email" : lowerCaseEmail,
+            "password" : password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
--- YallaChat/Utilities/Constants.swift
+++ YallaChat/Utilities/Constants.swift
@@ -8,9 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//MARK: URL Constants
+let BASE_URL = "https://yallachatting.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 // MARK: Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
 
-
+//MARK: User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
