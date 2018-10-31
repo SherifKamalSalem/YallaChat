bdee830ee5f087b71c6ee5cb25ecb17ddb41178c
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-10-31 22:19:50 +0200

    add timestamp

--- YallaChat/View/MessageCell.swift
+++ YallaChat/View/MessageCell.swift
@@ -33,6 +33,21 @@ class MessageCell: UITableViewCell {
         userNameTxt.text = message.userName
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        guard var isoDate = message.timeStamp else { return }
+        var end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 
 }
